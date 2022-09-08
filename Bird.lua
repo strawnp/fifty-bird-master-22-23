2065c99b4d1cdcda7917743cc78133d07da80696
Peter Strawn <peter.strawn@jesuithighschool.org>
2022-09-08 11:15:33 -0700

    bird-4

--- Bird.lua
+++ Bird.lua
@@ -1,6 +1,6 @@
 Bird = Class{}
 
-local GRAVITY = 20
+local GRAVITY = 15
 
 function Bird:init()
   self.image = love.graphics.newImage('bird.png')
@@ -15,6 +15,10 @@ end
 
 function Bird:update(dt)
   self.dy = self.dy + GRAVITY * dt
+
+  if love.keyboard.wasPressed('space') then
+    self.dy = -5
+  end
 
   self.y = self.y + self.dy
 end
--- main.lua
+++ main.lua
@@ -43,6 +43,8 @@ function love.load()
     fullscreen = false,
     resizable = true
   })
+
+  love.keyboard.keysPressed = {}
 end
 
 function love.resize(w, h)
@@ -50,8 +52,18 @@ function love.resize(w, h)
 end
 
 function love.keypressed(key)
+  love.keyboard.keysPressed[key] = true
+
   if key == 'escape' then
     love.event.quit()
+  end
+end
+
+function love.keyboard.wasPressed(key)
+  if love.keyboard.keysPressed[key] then
+    return true
+  else
+    return false
   end
 end
 
@@ -60,6 +72,8 @@ function love.update(dt)
   groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 
   bird:update(dt)
+
+  love.keyboard.keysPressed = {}
 end
 
 function love.draw()
